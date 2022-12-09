0500aa3d01c25c89e7d78cb61c3e22745f566663
Emmanuel Balikuddembe <balixemma@gmail.com>
2022-12-09 23:07:53 +0300

    Add calculator functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Buttons from './Buttons';
 import Output from './Output';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (event) => {
+    const values = calculate(this.state, event.target.innerText);
+    this.setState(values);
+  };
+
   render() {
     return (
       <div className="calculator">
-        <Output />
-        <Buttons />
+        <Output state={this.state} />
+        <Buttons handleClick={this.handleClick} />
       </div>
     );
   }

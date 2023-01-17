d4f3abbc17ddca4ab7491e83a805a34f3850c8b2
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-01-18 00:47:48 +0300

    Modify App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+// import Home from './components/Home';
+// import Nav from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      {/* <Home />
+      <Nav /> */}
     </div>
   );
 }
